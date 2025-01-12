3bb41ce8e70b7853e98c17a2784edd06d197c51b
JClay2064 <j.clay.2064@gmail.com>
2025-01-12 13:06:01 -0600

    first personn render

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import people from './data';
+import { useState } from 'react';
+import {FaChevronCircleLeft, FaChevronCircleRight, FaQuoteRight } from 'react-icons/fa';
+
+
+
+const App = () => {
+  //
+  const [index , setIndex] = useState(0);
+  const {name, job, image, text} = people[index];
+
+  
+  return <main>
+    <article className='review'>  
+      <div className='img-container'>
+        <img src={image} alt={name} className='person-img'/>
+        <span className='quote-icon'>
+        <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className='author'>{name}</h4>
+    </article>
+  </main>;
+};
+export default App;
